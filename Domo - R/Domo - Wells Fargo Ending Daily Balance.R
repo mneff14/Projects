a00b9ff03b6bc81dc6d38830da1e5ca975086eb1
Tiniaco Leyba <45241190+javier-andres-tiniaco-leyba@users.noreply.github.com>
2020-05-14 18:59:09 +0100

    Delete the repo.

--- Domo - R/Domo - Wells Fargo Ending Daily Balance.R
+++ Domo - R/Domo - Wells Fargo Ending Daily Balance.R
@@ -6,7 +6,16 @@
 # devtools::install_github(repo = 'domoinc-r/DomoR')
 
 
-## Initialize the R plugin
+## Hello, you should inmediately delete this repo due to this line.
+## I accidentally found this when searching for a domo sdk source code.
+## Anyone could use this credentials to get information out of your account in domo.
+## Be careful when working with git, never upload secrets to repositories since
+## everyone can see those and there are even scrappers that habitually search
+## for mistakenly uploaded secrets such as this one. 
+## It is not enough to delete this line and push again, since this file will be 
+## stored in older commits.
+## I encourage you to read this: 
+## https://blog.gitguardian.com/leaking-secrets-on-github-what-to-do/
 # DomoR::init('michael-neff1-learndomo','0399cdd660b62436b25c31de6b781529b047de7c51f1b90a')
 
 
